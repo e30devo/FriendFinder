513dd071e573cda689a6ab25e27ad1275a2beaba
e30devo <dthomase30@gmail.com>
2018-05-21 16:24:43 -0700

    Heroku launch test

--- server.js
+++ server.js
@@ -4,14 +4,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 require('./app/routing/apiRoutes.js')(app);
 require(path.join(__dirname,'./app/routing/htmlRoutes.js'))(app);
-
-// app.get("/", function (req, res) {
-//     res.sendFile(path.join(__dirname, "./app/public/home.html"));
-//     console.log(path.join(__dirname, "./app/public/home.html"));
-// });
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
