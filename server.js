07b769be7c905fb0d58906cfc58c5e1971f80afe
e30devo <dthomase30@gmail.com>
2018-05-24 17:41:02 -0700

    Issue line 169 of app/public/survey.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../public")));
-require('./app/routing/apiRoutes.js')(app);
+require(path.join(__dirname,'./app/routing/apiRoutes.js'))(app);
 require(path.join(__dirname,'./app/routing/htmlRoutes.js'))(app);
 
 app.listen(PORT, function () {
